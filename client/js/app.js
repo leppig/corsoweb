0c180391ad6475690c766a99a26557ab27b138cf
leppig <leppig@unimi.it>
2016-12-01 15:14:55 +0100

    pausa caff

--- client/js/app.js
+++ client/js/app.js
@@ -10,5 +10,15 @@ function($scope,Todo) {
 		desc:"",
 		important:false
 	}
+ $scope.aggiungitodo=function(){
+ 	console.log("Aggiungo: %o",$scope.newTodo);
+ }
 
+ $scope.cancellatodo=function(idtodo){
+ 	console.log("cancello: %o",idtodo);
+ }
+
+ $scope.eseguitodo=function(idtodo){
+ 	console.log("esegui: %o",idtodo);
+ }
 }]);
